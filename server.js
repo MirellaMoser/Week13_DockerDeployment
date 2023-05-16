987e7d61a5517a0c63ba54b92d539de3ce2461a6
MirellaMoser <mirella.moser@gmx.net>
2023-05-16 18:16:02 +0200

    test2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course. This is a test.');
+    res.send('Hello DevOps Course. This is a test, again:)');
 });
 
 app.listen(PORT, HOST);
